7a79fa2bb7ec7ba9cde8cd03e5e51904a210de6f
Christen Lofland <ChristenLofland@gmail.com>
2021-02-28 14:32:36 -0600

    Improve Maestro script

--- scripts/maestro_set_target.sh
+++ scripts/maestro_set_target.sh
@@ -16,19 +16,37 @@
 # so you need to multiply whatever your microseconds are by four,
 # and no decimals are allowed.
 
-DEVICE=$1
-CHANNEL=$2
-TARGET=$3
-
 byte() {
-  printf "\\x$(printf "%x" $1)"
+  printf "\\x$(printf "%x" "$1")"
 }
 
-stty raw -F $DEVICE
+if [[ -z $3 ]]; then
+  printf "Usage: maestro-set-target.sh DEVICE CHANNEL TARGET\n"
+  printf "Linux example: bash maestro-set-target.sh /dev/ttyACM0 0 6000\n"
+  printf "\n"
+  printf "CHANNEL is the channel number\n"
+  printf "TARGET is the target in units of quarter microseconds.\n"
+  printf "\n"
+  printf "The Maestro must be configured to be in USB Dual Port mode.\n"
+  printf "\n"
+  printf "You can use the find_maestro.sh command to find the port:\n"
+  printf "./maestro_set_target.sh \$(./find_Maestro.sh) 0 2000\n"
+  printf "\n"
+  printf "NOTICE that the increment is \"quarter microseconds\",\n"
+  printf "so you need to multiply whatever your microseconds are by four,\n"
+  printf "and no decimals are allowed.\n"
+else
 
-{
-  byte 0x84
-  byte $CHANNEL
-  byte $((TARGET & 0x7F))
-  byte $((TARGET >> 7 & 0x7F))
-} > $DEVICE
+  DEVICE=$1
+  CHANNEL=$2
+  TARGET=$3
+
+  stty raw -F "$DEVICE"
+
+  {
+    byte 0x84
+    byte "$CHANNEL"
+    byte $((TARGET & 0x7F))
+    byte $((TARGET >> 7 & 0x7F))
+  } >"$DEVICE"
+fi
