27a325d8afe46fe7bcf8a972b0de1cd7f56379ec
Christen Lofland <ChristenLofland@gmail.com>
2022-01-01 21:13:10 -0600

    Reset servo to 0 after setting to avoid strain.

--- website/src/components/Servo.js
+++ website/src/components/Servo.js
@@ -13,6 +13,17 @@ const Servo = (props) => {
     }
   };
 
+  const handleSliderAfterChange = () => {
+    // A 0 to a regular servo just means "turn off". The servo will stay at the
+    // last position if it isn't under strain.
+    // This prevents the servo from drawing current continuously.
+    // We are NOT setting the slider position to 0, because, again 0 turns
+    // the servo off, it does not move it.
+    if (props.socket) {
+      props.socket.emit('servo360', { target: props.name, value: 0 });
+    }
+  };
+
   let sliderStyle = { width: 300 };
   if (props.vertical) {
     sliderStyle = { height: 300 };
@@ -31,6 +42,7 @@ const Servo = (props) => {
           min={-1000}
           max={1000}
           onChange={handleSliderChange}
+          onAfterChange={handleSliderAfterChange}
           vertical={props.vertical}
         />
       </div>
