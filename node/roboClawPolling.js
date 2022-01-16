4cf8f0c9c9264b9580232b26eaa76cc01fd6ccb4
Christen Lofland <ChristenLofland@gmail.com>
2022-01-16 16:24:54 -0600

    Improve Roboclaw polling and add more data.

--- node/roboClawPolling.js
+++ node/roboClawPolling.js
@@ -3,36 +3,37 @@
 import roboClawDataHandler from './roboClawDataHandler.js';
 import wait from './wait.js';
 
-const pollingInterval = 5000; // Milliseconds
-const minimumTimeSinceLastCommandSent = 500; // Milliseconds
+const pollingInterval = 1000; // Milliseconds
 
 async function pollRoboClawData(roboClaw) {
   roboClaw.connection.send({
     command: 'GETMBATT',
     callback: roboClawDataHandler,
   });
-  await wait(pollingInterval);
+  roboClaw.connection.send({
+    command: 'GETCURRENTS',
+    callback: roboClawDataHandler,
+  });
   roboClaw.connection.send({
     command: 'GETTEMP',
     callback: roboClawDataHandler,
   });
-  await wait(pollingInterval);
   roboClaw.connection.send({
     command: 'GETTEMP2',
     callback: roboClawDataHandler,
   });
-  await wait(pollingInterval);
-  // TODO:
-  // 'GETCURRENTS'
-  // 'GETERROR'
+  roboClaw.connection.send({
+    command: 'GETERROR',
+    callback: roboClawDataHandler,
+  });
 }
 
 async function roboClawPolling(roboClaw) {
   while (1) {
     // eslint-disable-next-line no-await-in-loop
-    await wait(pollingInterval);
-    // eslint-disable-next-line no-await-in-loop
     await pollRoboClawData(roboClaw);
+    // eslint-disable-next-line no-await-in-loop
+    await wait(pollingInterval);
   }
 }
 
