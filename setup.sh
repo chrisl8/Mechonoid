54babf28980dd0f6e8347f2526b4f3ae94697213
Christen Lofland <Christen@Lofland.net>
2022-03-16 14:00:38 -0500

    Add a line break to root check.

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ if ! (grep "DISTRIB_ID=Ubuntu" /etc/lsb-release >/dev/null) || ! (grep "DISTRIB_
 fi
 
 if [[ ${USER} == "root" ]]; then
-  printf "${RED}[This script must not be run as root]${NC}\n"
+  printf "\n${RED}[This script must not be run as root]${NC}\n"
   printf "${RED}exit root back to your normal user and try again.${NC}\n"
   exit 1
 fi
